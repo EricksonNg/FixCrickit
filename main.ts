19ad6a16e907ac1d51dafc29a88f38c7986dfb71
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2022-02-16 14:32:51 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        crickit.motor2.run(100)
+        basic.showIcon(IconNames.Sad)
+    }
+    if (receivedNumber == 1) {
+        crickit.motor2.run(0)
+        basic.showIcon(IconNames.TShirt)
+    }
+})
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "GO") {
+    if (receivedString == "R") {
         crickit.motor1.run(100)
-        basic.showIcon(IconNames.House)
-    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (receivedString == "STOP") {
         crickit.motor1.run(0)
-        basic.showIcon(IconNames.Tortoise)
+        basic.showIcon(IconNames.Chessboard)
     }
 })
 radio.setGroup(158)
