b8adf6097c4020432a99969a0723e6d12844e8b3
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2022-02-17 12:50:59 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        crickit.motor2.run(100)
-        basic.showIcon(IconNames.Sad)
-    }
-    if (receivedNumber == 1) {
-        crickit.motor2.run(0)
-        basic.showIcon(IconNames.TShirt)
-    }
-})
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "R") {
+    if (receivedString == "Both") {
         crickit.motor1.run(100)
-        basic.showIcon(IconNames.Sad)
-    }
-    if (receivedString == "STOP") {
-        crickit.motor1.run(0)
-        basic.showIcon(IconNames.Chessboard)
+        crickit.motor2.run(100)
+    } else {
+        if (receivedString == "R") {
+            crickit.motor1.run(100)
+            basic.showIcon(IconNames.Sad)
+        }
+        if (receivedString == "SR") {
+            crickit.motor1.run(0)
+            basic.showIcon(IconNames.Chessboard)
+        }
+        if (receivedString == "S") {
+            crickit.motor2.run(100)
+            basic.showIcon(IconNames.Sad)
+        }
+        if (receivedString == "SL") {
+            crickit.motor2.run(0)
+            crickit.motor2.stop()
+            basic.showIcon(IconNames.TShirt)
+        }
     }
 })
 radio.setGroup(158)
